436d8a2a50226b94eaae149b756b05e57f2a89f3
unknown <matinkhaled79@gmail.com>
2025-05-09 14:18:32 +0300

    Fix Jenkinsfile for Docker and update dependencies

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
 
+test('App should be defined', () => {
+    expect(app).toBeDefined();
+});
+
 describe('GET /', () => {
-    it('should respond with a 200 status code', async () => {
+    it('should respond with a 200 status code', async() => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
     });
 
-    it('should return a welcome message', async () => {
+    it('should return a welcome message', async() => {
         const response = await request(app).get('/');
         expect(response.text).toContain('Welcome to the application!');
     });
